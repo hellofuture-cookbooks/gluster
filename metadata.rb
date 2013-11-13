a7161d3a699d17426f515cf9b110cee1adbefa4e
Andy Gale <andy@hellofutu.re>
2013-11-13 12:03:50 +0000

    Gluster cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'gluster'
+maintainer       'Biola University'
+maintainer_email 'jared.king@biola.edu'
+license          'Apache 2.0'
+description      'Installs and configures Gluster servers and clients'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.3.1'
+depends          'apt'
